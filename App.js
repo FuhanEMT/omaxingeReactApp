b6c7847879cc7eaa78de2f189865bbb5cf9e4090
panda.li <panda.li@hg2mes.com>
2022-01-07 15:58:35 +0800

    fix:up

--- App.js
+++ App.js
@@ -6,18 +6,38 @@
  * @flow strict-local
  */
 // React Import Modules
-import React, { Component , Node } from 'react';
+import React, { Component, Node } from 'react';
 
 // React-Native Import Modules
 import {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  Image
 } from 'react-native';
 
 const styles = StyleSheet.create({
   header: {
-    padding: 20
+    paddingLeft: 15,
+    paddingTop: 18,
+    paddingBottom: 15,
+    borderColor: '#e5e5e5',
+    borderBottomWidth: 1,
+    // 样式继承
+    text: {
+      color: '#000',
+      marginRight: '30%'
+    },
+    LoginBox: {
+      flex: 2,
+      flexDirection: "row",
+      flexWrap: "wrap",
+      box: {
+        marginRight: 20,
+      }
+    },
+    flexDirection: "row",
+    flexWrap: "wrap",
   }
 })
 
@@ -25,7 +45,27 @@ class Header extends Component {
   render() {
     return (
       <View style={styles.header}>
-        <Text>头部</Text>
+        <Text style={styles.header.text}>Welcome to Omaxinge App</Text>
+        <View style={styles.header.LoginBox}>
+          <View style={styles.header.LoginBox.box}>
+            <Text>Log in</Text>
+          </View>
+          <View>
+            <Text>Set Up</Text>
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
+
+class BannerImages extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={{
+          uri: 'https://facebook.github.io/react/logo-og.png',
+        }} style={{ width: 100, height: 100 }} />
       </View>
     )
   }
@@ -37,6 +77,7 @@ const App: () => Node = () => {
   return (
     <View>
       <Header />
+      <BannerImages />
     </View>
 
   );
